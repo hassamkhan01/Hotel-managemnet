7e85fc0254da92473d5a9385e72e0b1e24279f68
hassamkhan01 <hassamk12345@gmail.com>
2024-10-03 16:26:56 +0500

    first commit

---
+++ HotelManagmentSystem/Controllers/RoomController.cs
@@ -0,0 +1,67 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using HotelManagmentSystem.Models;
+using HotelManagmentSystem.ViewModel;
+using System.IO;
+
+
+namespace HotelManagmentSystem.Controllers
+{
+    public class RoomController : Controller
+    {
+        private HotelDBEntities objHotelDBEntities;
+
+        public RoomController()
+        {
+            objHotelDBEntities = new HotelDBEntities();
+        }
+
+        public ActionResult Index()
+        {
+            RoomViewModel objRoomViewModel = new RoomViewModel();
+            objRoomViewModel.ListOfBookingStatus = (from obj in objHotelDBEntities.BookingStatus
+                                                    select new SelectListItem()
+                                                    {
+                                                        Text = obj.Bookingstatus,
+                                                        Value = obj.BookingstatusId.ToString()
+                                                    }).ToList();
+
+            objRoomViewModel.ListOfRoomType = (from obj in objHotelDBEntities.RoomTypes
+                                               select new SelectListItem()
+                                               {
+                                                   Text = obj.RoomTypeName,
+                                                   Value = obj.RoomTypeId.ToString()
+                                               }).ToList();
+
+            return View(objRoomViewModel);
+
+        }
+        [HttpPost]
+        public ActionResult Index(RoomViewModel objRoomViewModel)
+        {
+            string ImageUniqueName = Guid.NewGuid().ToString();
+            string ActualImageName = ImageUniqueName + Path.GetExtension(objRoomViewModel.Image.FileName);
+
+            objRoomViewModel.Image.SaveAs(filename: Server.MapPath("~/RoomImages/" + ActualImageName));
+            //objHotelDBEntities
+            Room objRoom = new Room()
+            {
+                RoomNumber = objRoomViewModel.RoomNumber,
+                RoomDescription = objRoomViewModel.RoomDescription,
+                RoomPrice = objRoomViewModel.RoomPrice,
+                BookingStatusId = objRoomViewModel.BookingStatusId,
+                RoomImage = ActualImageName,
+                RoomCapacity = objRoomViewModel.RoomCapacity,
+                RoomTypeId = objRoomViewModel.RoomTypeId
+            };
+            objHotelDBEntities.Rooms.Add(objRoom);
+            objHotelDBEntities.SaveChanges();
+            return Json(data:new { message = "Room Succesfully Added.", success = true }, JsonRequestBehavior.AllowGet);
+
+        }
+        
+    }
+    }
---
+++ HotelManagmentSystem/Scripts/ViewModel/SelectlistItem.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using HotelManagmentSystem.ViewModel;
+
+namespace HotelManagmentSystem.ViewModel
+{
+    public class SelectlistItem
+    {
+             
+    }
+}
